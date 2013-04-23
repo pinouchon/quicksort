4575180b9d303e90a25d7f8cd57c565c6c3864f5
sharewizz <sharewizz@sharewizzs-MacBook-Pro.local>
2013-04-23 22:46:11 +0200

    initial commit. Empty rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Quicksort::Application.config.secret_token = '19c6095c36afb5ff0c8c98d254bc9c4b11490922309cfcb12d3b50b38039346d7e595e9d23aa69607aa991b3eefd290fdb10e4497e472c88581dd5d050af7471'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Quicksort::Application.config.session_store :cookie_store, key: '_quicksort_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Quicksort::Application.config.session_store :active_record_store
